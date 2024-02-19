a6672831d1c3b7db38fbd669dd45b272e158b4c5
shaunpetro <shaun.p.mckoy@gmail.com>
2024-02-19 13:49:41 +0000

    task 0 files

--- sort.h
+++ sort.h
@@ -43,6 +43,7 @@ typedef struct listint_s
 /* ************** project files prototypes ************** */
 void insertion_sort_list(listint_t **list);
 void bubble_sort(int *array, size_t size);
-
+void print_list(const listint_t *list);
+void print_array(const int *array, size_t size);
 
 #endif
